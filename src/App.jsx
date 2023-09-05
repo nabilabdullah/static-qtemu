5366165608841904c2cd1f71c726ee5ef18d997a
nabilabdullah <nabil.abdullah28@gmail.com>
2023-09-05 14:01:14 +0700

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,19 @@ function App() {
         <div class="collapse navbar-collapse ml-5" id="navbarText">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Create Meetup <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#create-meetup">Create Meetup <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item ml-3">
-              <a class="nav-link" href="#">Explore</a>
+              <a class="nav-link" href="#next-meetup">Next Meetup</a>
+            </li>
+            <li class="nav-item ml-3">
+              <a class="nav-link" href="#about-meetup">About Meetup</a>
+            </li>
+            <li class="nav-item ml-3">
+              <a class="nav-link" href="#members">Members</a>
+            </li>
+            <li class="nav-item ml-3">
+              <a class="nav-link" href="#past-meetups">Past Meetups</a>
             </li>
           </ul>
           <span class="navbar-text">
@@ -25,7 +34,7 @@ function App() {
       </nav>
 
       {/* HERO */}
-      <div class="bg-light m-3">
+      <div class="bg-light m-3" id="create-meetup">
         <div class="row">
           <div class="col-4">
             <img src="meetup.jpg" width="100%" class="m-3" alt="ini foto" />
@@ -66,25 +75,20 @@ function App() {
       </div>
 
       {/* Next Meetup */}
-      <div class="m-3 mt-5">
+      <div class="m-3 mt-5" id="next-meetup">
         <p class="text-xl font-weight-bold"><big>Next Meetup</big></p>
         <div class="bg-light m-2 p-2">
           <p class="font-weight-bold mt-3">Awesome Meetup and Event</p>
           <p class="font-weight-bold text-warning">28 Oktober 2023</p>
           <div class="row">
-            <div class="col-12">Hello JavaScript and React Js</div>
-            <div class="col-12">Get ready for our monthly meetup PontianakJS! This will be our fifth meetup of 2023!</div>
-            <div class="col-12">The meetup format will contain some short stories and technical talks.</div>
-            <div class="col-12">If you have a short announcement you'd like to share with the audience, you may do so during open mic announcements.</div>
+            <div class="col-12 text-justify">Hello JavaScript and React Js. Get ready for our monthly meetup PontianakJS! This will be our fifth meetup of 2023! The meetup format will contain some short stories and technical talks. If you have a short announcement you'd like to share with the audience, you may do so during open mic announcements. Hello JavaScript and React Js. Get ready for our monthly meetup PontianakJS! This will be our fifth meetup of 2023! The meetup format will contain some short stories and technical talks. If you have a short announcement you'd like to share with the audience, you may do so during open mic announcements. Remember to bring a photo ID to get through building security.</div>
           </div>
-          <p class="mt-3">Remember to bring a photo ID to get through building security.</p>
-          <p class="mt-3">---</p>
           <p class="mt-3">See you there!</p>
         </div>
       </div>
 
       {/* About Meetup */}
-      <div class="m-3 mt-5">
+      <div class="m-3 mt-5" id="about-meetup">
         <p class="text-xl font-weight-bold"><big>About Meetup</big></p>
         <div class="m-2 p-2">
           <p class="">Come and meet other developers interested in the JavaScript and it's library in the Greater Pontianak area.</p>
@@ -93,13 +97,13 @@ function App() {
       </div>
 
       {/* Members */}
-      <div class="m-3 mt-5">
+      <div class="m-3 mt-5" id="members">
         <div class="row">
           <div class="col-6">
             <p class="font-weight-bold"><big>Members</big></p>
           </div>
           <div class="col-6 text-right">
-            <p>See all</p>
+            <p class="text-danger">See all</p>
           </div>
         </div>
         <div class="row bg-light mx-2 p-2">
@@ -123,13 +127,13 @@ function App() {
       </div>
 
       {/* Past Meetups */}
-      <div class="m-3 mt-5">
+      <div class="m-3 mt-5" id="past-meetups">
         <div class="row">
           <div class="col-6">
             <p class="font-weight-bold"><big>Past Meetups</big></p>
           </div>
           <div class="col-6 text-right">
-            <p>See all</p>
+            <p class="text-danger">See all</p>
           </div>
         </div>
       </div>
